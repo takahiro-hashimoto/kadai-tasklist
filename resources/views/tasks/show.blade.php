f4826d912df2cc274c57acf0368482ef8b39eb20
takahiro.hashimoto <bridge.takahiro.1539@gmail.com>
2024-03-02 15:49:30 +0000

    CRUDの実装

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="prose ml-4">
+        <h2>id = {{ $task->id }} の詳細ページ</h2>
+    </div>
+
+    <table class="table w-full my-4">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+
+        <tr>
+            <th>やること</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    
+    <a class="btn btn-outline" href="{{ route('tasks.edit', $task->id) }}">このタスクを編集</a>
+    
+@endsection
